79fe542b2a25c991fc6e3fb6d6529609a9e25c18
Kevin Nguyen <cheesy101@hotmail.com>
2025-08-13 21:09:14 +1000

    Added live IP to axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: '13.211.228.247', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
