4c2ced3f87ab23f9dea5f891cad058de78721162
Kevin Nguyen <cheesy101@hotmail.com>
2025-08-14 15:39:57 +1000

    Fixed build bug in ci.yml file

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: '13.211.21.176', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: '13.211.21.176', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
