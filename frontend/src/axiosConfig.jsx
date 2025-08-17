448581e06ae1b64784b0cf6524993e18d42e4ee6
Kevin Nguyen <cheesy101@hotmail.com>
2025-08-17 19:32:33 +1000

    change IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.63.220.77:5001', // live
+  baseURL: 'http://3.106.53.149:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
