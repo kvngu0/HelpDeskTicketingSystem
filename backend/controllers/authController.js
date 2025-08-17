d028aa72741ad6e0796b3bfb087b3d3b0bea9ed2
Kevin Nguyen <cheesy101@hotmail.com>
2025-08-17 17:34:13 +1000

    changing head

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,6 +32,7 @@ const loginUser = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
+    
 };
 
 const getProfile = async (req, res) => {
